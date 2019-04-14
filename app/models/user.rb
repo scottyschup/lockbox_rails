581e5ddaeaceed8802a3ef9952307f0c39c09c45
Timothy Meixell <misternu@gmail.com>
2019-04-14 15:23:06 -0500

    add devise

---
+++ app/models/user.rb
@@ -0,0 +1,6 @@
+class User < ApplicationRecord
+  # all but :timeoutable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable,
+         :confirmable, :lockable, :trackable
+end
