ea93f9a5f283faaac6f3e2d4b7bf4025c1f607c6
Kevin Yaroch <kjyaroch@gmail.com>
2019-04-21 16:41:55 -0500

    Add SupportRequest validations

--- app/models/support_request.rb
+++ app/models/support_request.rb
@@ -2,4 +2,20 @@ class SupportRequest < ApplicationRecord
   belongs_to :lockbox_partner
   belongs_to :user
   has_many :lockbox_actions
+
+  UUID_REGEX = /\A[0-9a-f]{8}-[0-9a-f]{4}-[1-5][0-9a-f]{3}-[89ab][0-9a-f]{3}-[0-9a-f]{12}\z/
+
+  validates :client_ref_id, presence: true, format: { with: UUID_REGEX }
+  validates :name_or_alias, presence: true
+  validates :user, presence: true
+  validates :lockbox_partner, presence: true
+
+  # Sometimes the UUID will already have been created elsewhere, and sometimes not
+  before_validation :populate_client_ref_id
+
+  private
+
+  def populate_client_ref_id
+    self.client_ref_id = SecureRandom.uuid if client_ref_id.blank?
+  end
 end
