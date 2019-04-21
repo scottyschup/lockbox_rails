9a5bbfff56f8dc9f046ab61a5f84284507eda612
BintLopez <nmlopez21@gmail.com>
2019-04-21 17:24:18 -0500

    add placeholder factories, add missing model tests

--- spec/models/lockbox_action_spec.rb
+++ spec/models/lockbox_action_spec.rb
@@ -3,4 +3,5 @@ require 'rails_helper'
 describe LockboxAction, type: :model do
   it { is_expected.to belong_to(:lockbox_partner) }
   it { is_expected.to have_many(:lockbox_transactions) }
+  it { is_expected.to have_many(:notes) }
 end
