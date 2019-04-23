fdaa46e739df15970cbc65e90d132fa22e4522f6
BintLopez <nmlopez21@gmail.com>
2019-04-23 15:48:40 -0500

    spike out lockbox action and transaction creation

--- app/models/lockbox_action.rb
+++ app/models/lockbox_action.rb
@@ -3,9 +3,59 @@ class LockboxAction < ApplicationRecord
   has_many :lockbox_transactions
   has_many :notes, as: :notable
 
+  STATUSES = [
+    PENDING   = 'pending',
+    COMPLETED = 'completed',
+    CANCELED  = 'canceled'
+  ].freeze
+
   ACTION_TYPES = [
     :add_cash,
     :reconcile,
     :support_client
   ].freeze
+
+  # action_type should correspond with ACTION_TYPES
+  def self.create_with_transactions(action_type, params)
+    if !ACTION_TYPES.include?(action_type)
+      raise ArgumentError.new("The only Lockbox actions supported are #{ACTION_TYPES.join(',')}")
+    else
+      ActiveRecord::Base.transaction do
+        lockbox_action = create!(
+          eff_date: params[:date],
+          action_type: action_type,
+          status: PENDING
+        )
+
+        case action_type
+        when :add_cash
+          lockbox_action.lockbox_transactions.create!(
+            eff_date: params[:date],
+            amount_cents: params[:amount_cents],
+            balance_effect: 'credit'
+          )
+        when :reconcile
+          expected_amount = lockbox_partner.expected_cash_in_box.cents
+          return if expected_amount == params[:amount_cents]
+          balance_effect = expected_amount > params[:amount_cents] ? 'debit' : 'credit'
+
+          lockbox_action.lockbox_transactions.create!(
+            eff_date: params[:date],
+            amount_cents: params[:amount_cents],
+            balance_effect: balance_effect,
+            category: 'adjustment'
+          )
+        when :support_client
+          params[:cost_breakdown].each do |item|
+            lockbox_action.lockbox_transactions.create!(
+              eff_date:       params[:date],
+              amount_cents:   item[:amount_cents],
+              balance_effect: 'debit',
+              category:       item[:category]
+            )
+          end
+        end
+      end
+    end
+  end
 end
--- spec/models/lockbox_action_spec.rb
+++ spec/models/lockbox_action_spec.rb
@@ -4,4 +4,30 @@ describe LockboxAction, type: :model do
   it { is_expected.to belong_to(:lockbox_partner) }
   it { is_expected.to have_many(:lockbox_transactions) }
   it { is_expected.to have_many(:notes) }
+
+  context '.create_with_transactions' do
+    context 'some unknown action type' do
+      # it raises some meaningful error
+    end
+
+    context 'add_cash' do
+      # given an amount and a date
+      # creates a corresponding credit txn
+      # no category
+    end
+
+    context 'reconcile' do
+      # given an amount and a date
+      # compares amount to balance
+      # creates an adjustment if amount counted != amount expected
+      # (credit if amount counted > amount expected)
+      # (debit if amount counted < amount expected)
+    end
+
+    context 'support_client' do
+      # given an amount, a date, and a cost breakdown
+      # cost breakdown is an array of hashes containing amount & category
+      # may create multiple debit txns depending on items in cost breakdown
+    end
+  end
 end
