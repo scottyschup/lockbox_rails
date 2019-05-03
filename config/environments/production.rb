b7c5dfe59778cfc757fe03501aa21ea809c38c86
BintLopez <nmlopez21@gmail.com>
2019-05-03 18:40:57 -0500

    add app url to config

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -60,7 +60,6 @@ Rails.application.configure do
   # config.active_job.queue_adapter     = :resque
   # config.active_job.queue_name_prefix = "lockbox_rails_production"
 
-  config.action_mailer.default_url_options = { host: ENV['HOST'] }
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
     :authentication => :plain,
@@ -70,6 +69,10 @@ Rails.application.configure do
     :user_name => ENV['MG_USERNAME'],
     :password => ENV['MG_PASSWORD']
   }
+
+  config.action_mailer.default_url_options = { :host => 'https://mac-lockbox.herokuapp.com/' }
+  config.action_mailer.asset_host = 'https://mac-lockbox.herokuapp.com/'
+  config.action_controller.asset_host = 'https://mac-lockbox.herokuapp.com/'
   config.action_mailer.perform_caching = false
 
   # Ignore bad email addresses and do not raise email delivery errors.
