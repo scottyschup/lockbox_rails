a85e81a34eccfe7f12d4c8b6775eebf17df15b39
Kevin Yaroch <kjyaroch@gmail.com>
2019-05-19 13:54:03 -0500

    Update specs to handle active lockbox partner requirement

--- spec/factories/lockbox_actions.rb
+++ spec/factories/lockbox_actions.rb
@@ -4,15 +4,21 @@ FactoryBot.define do
     status          { 'pending' }
     lockbox_partner
 
+    trait :active_lockbox_partner do
+      association :lockbox_partner, :active
+    end
+
     trait :add_cash do
       action_type   { 'add_cash' }
     end
 
     trait :reconciliation do
+      active_lockbox_partner
       action_type   { 'reconcile' }
     end
 
     trait :support_client do
+      active_lockbox_partner
       action_type   { 'support_client' }
       association :support_request
     end
