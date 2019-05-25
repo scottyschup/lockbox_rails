e110cc7348b18099f10177d612d851ddca8e9f08
BintLopez <nmlopez21@gmail.com>
2019-05-25 17:00:05 -0500

    revert changes to support req

--- app/models/support_request.rb
+++ app/models/support_request.rb
@@ -11,20 +11,6 @@ class SupportRequest < ApplicationRecord
   # Sometimes the UUID will already have been created elsewhere, and sometimes not
   before_validation :populate_client_ref_id
 
-  def self.create_with_action(params)
-    ActiveRecord::Base.transaction do
-      create!(
-        lockbox_partner_id: params[:lockbox_partner_id],
-        name_or_alias: params[:name_or_alias],
-        user_id: params[:user_id]
-      ).tap do |support_request|
-        support_request.lockbox_actions.create_with_transactions(
-          LockboxAction::SUPPORT_CLIENT, params
-        )
-      end
-    end
-  end
-
   private
 
   def populate_client_ref_id
