9d56342f476543ebd2bb3b3883385b080a9a69d3
Kevin Yaroch <kjyaroch@gmail.com>
2019-05-26 15:10:42 -0500

    Use correct layout on failed form submission

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -4,7 +4,7 @@ class Users::RegistrationsController < Devise::RegistrationsController
   # before_action :configure_sign_up_params, only: [:create]
   before_action :configure_account_update_params, only: [:update]
 
-  layout 'application', only: :edit
+  layout 'application', only: [:edit, :update]
 
   # GET /resource/sign_up
   # def new
