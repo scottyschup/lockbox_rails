b542675b20d183935a670c9d56664f2d09e03db4
Sree P <34173394+nsiwnf@users.noreply.github.com>
2019-06-02 13:28:25 -0500

    Update db/seeds.rb
    Co-Authored-By: Nicole <nmlopez21@gmail.com>

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,7 @@
 mac_user = User.where(email: 'cats@test.com').first_or_create!(
   password: 'password1234',
   confirmed_at: Time.current,
-  role: 'admin'
+  role: User::ADMIN
 )
 
 LOCKBOX_PARTNERS = [
