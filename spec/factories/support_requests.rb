c0b8dbd3d5445083c1cae5325df59c48bf0348e2
BintLopez <nmlopez21@gmail.com>
2019-07-02 01:04:13 -0500

    fix failures maybe

--- spec/factories/support_requests.rb
+++ spec/factories/support_requests.rb
@@ -2,7 +2,6 @@ FactoryBot.define do
   factory :support_request do
     association    :user
     association    :lockbox_partner
-    association    :lockbox_action
     name_or_alias  { Faker::Name.first_name }
     client_ref_id  { Faker::Alphanumeric.alpha(8) }
   end
