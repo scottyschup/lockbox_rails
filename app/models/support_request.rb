f6a2ee11d789f9a2c1460b79dbdd49ddc8789e04
Chris Doyle <chris@arches.io>
2019-08-28 20:51:16 -0500

    comment for greppability

--- app/models/support_request.rb
+++ app/models/support_request.rb
@@ -12,6 +12,10 @@ class SupportRequest < ApplicationRecord
   # Sometimes the UUID will already have been created elsewhere, and sometimes not
   before_validation :populate_client_ref_id
 
+  # for greppability:
+  # scope :pending
+  # scope :completed
+  # scope :canceled
   LockboxAction::STATUSES.each do |status|
     scope status, -> { joins(:lockbox_action).where("lockbox_actions.status": status) }
     scope "#{status}_for_partner", ->(lockbox_partner_id:) { joins(:lockbox_action).where(lockbox_partner_id: lockbox_partner_id, "lockbox_actions.status": status) }
