f665bcb871968b8dff24be9ee37855d0e1f679d0
BintLopez <nmlopez21@gmail.com>
2019-08-31 10:19:29 -0500

    fix merge conflict leftover

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,9 +1,11 @@
 class ApplicationController < ActionController::Base
   before_action :authenticate_user!
 
-  def ensure_admin_only!  if !current_user.admin? && current_user.lockbox_partner != @lockbox_partner
-    flash[:error] = "You are not authorized to access this page"
-    return redirect_to root_path
+  def ensure_admin_only!
+    if !current_user.admin? && current_user.lockbox_partner != @lockbox_partner
+      flash[:error] = "You are not authorized to access this page"
+      return redirect_to root_path
+    end
   end
 
   def require_admin
