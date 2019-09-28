7bb7f57a59b6557cfbdd95e701a92f2638cc9f7c
Timothy Meixell <misternu@gmail.com>
2019-09-28 12:24:08 -0500

    refactor notes js and enable cancel button

--- app/javascript/src/notes.js
+++ app/javascript/src/notes.js
@@ -1,23 +1,41 @@
+const addNewNote = note => {
+  $('tbody').prepend(note);
+};
+
+const openNoteForm = () => {
+  $('#new-note-form').slideDown(250);
+  $('#new-note')
+    .parent()
+    .addClass('selected');
+  $('#new-note-form textarea').focus();
+};
+
+const clearNoteForm = () => {
+  $('#new-note-form').slideUp(250);
+  $('#new-note-form textarea').val('');
+  $('#new-note')
+    .parent()
+    .removeClass('selected');
+};
+
 const handleNoteResponse = response => {
   const data = response.detail[0];
   if (data.note) {
-    $('tbody').prepend(data.note);
-    $('#new-note-form').slideUp(250);
-    $('#new-note-form textarea').val('');
-    $('#new-note')
-      .parent()
-      .removeClass('selected');
+    addNewNote(data.note);
+    clearNoteForm();
   }
 };
 
 const setupNotes = () => {
   const notesLog = $('.notes-log');
   if (notesLog) {
-    const newNoteButton = $('#new-note');
-    newNoteButton.on('click', event => {
+    $('#new-note').on('click', event => {
       event.preventDefault();
-      $('#new-note-form').slideDown(250);
-      newNoteButton.parent().addClass('selected');
+      openNoteForm();
+    });
+    $('#cancel-note').on('click', event => {
+      event.preventDefault();
+      clearNoteForm();
     });
     document.removeEventListener('ajax:success', handleNoteResponse);
     document.addEventListener('ajax:success', handleNoteResponse);
