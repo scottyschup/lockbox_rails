cd4662704f77c4ce4a4a3f794829f8ac48149e67
Kevin Yaroch <kjyaroch@gmail.com>
2019-10-13 18:16:16 -0500

    Add mailer and template

---
+++ app/mailers/support_request_mailer.rb
@@ -0,0 +1,12 @@
+class SupportRequestMailer < ApplicationMailer
+  def creation_alert
+    @support_request = params[:support_request]
+
+    email_addresses = @support_request.lockbox_partner.users.confirmed.pluck(:email)
+    return if email_addresses.empty?
+
+    subject = "A new Support Request was sent to #{@support_request.lockbox_partner.name}"
+
+    mail(to: email_addresses, subject: subject, bcc: @support_request.creator.email)
+  end
+end
