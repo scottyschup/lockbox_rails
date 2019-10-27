f4f5ec1c3a4fa8b7589ea65eb8251ad5620bf45e
Kevin Yaroch <kjyaroch@gmail.com>
2019-10-27 17:34:43 -0500

    Email coordinators and CC partner users for note created by partner user

--- app/mailers/support_request_mailer.rb
+++ app/mailers/support_request_mailer.rb
@@ -24,14 +24,17 @@ class SupportRequestMailer < ApplicationMailer
       raise ArgumentError, "This note is not associated with a support request"
     end
 
-    return if partner_user_emails.empty?
     subject = "A new note was added to Support Request ##{@support_request.id}"
     coordinator_emails = [@support_request.user.email, @note.user.email].uniq
-    mail(
-      to: partner_user_emails,
-      subject: subject,
-      cc: coordinator_emails
-    )
+    # If a coordinator creates the note, email the partner users and CC the
+    # coordinator(s). If a partner user creates it, do the reverse
+    to_emails, cc_emails = if @note.user.admin?
+      [partner_user_emails, coordinator_emails]
+    else
+      [coordinator_emails, partner_user_emails]
+    end
+
+    mail(to: to_emails, subject: subject, cc: cc_emails)
   end
 
   private
