397d411107eccd4e00e2c11384aa51a813901f94
AG-Sile <annaglusz@gmail.com>
2019-10-26 19:53:30 -0500

    fix negative recon amounts

--- spec/lib/reconciliation_spec.rb
+++ spec/lib/reconciliation_spec.rb
@@ -30,6 +30,32 @@ describe Reconciliation do
         .and_return(Monetize.parse('1000.00'))
     end
 
+    context 'when the amount is less than lockbox balance' do
+      let(:amount) { Money.new(900_00) }
+
+      it 'creates one lockbox action' do
+        expect{reconcile}.to change(LockboxAction, :count).by(1)
+      end
+
+      it 'creates the lockbox action with the correct attributes' do
+        lockbox_action = reconcile.value
+        expect(lockbox_action.lockbox_partner_id).to eq(lockbox_partner.id)
+        expect(lockbox_action.action_type).to eq(LockboxAction::RECONCILE)
+        expect(lockbox_action.status).to eq(LockboxAction::COMPLETED)
+      end
+
+      it 'creates one lockbox transaction' do
+        expect{reconcile}.to change(LockboxTransaction, :count).by(1)
+      end
+
+      it 'creates the lockbox transaction with the correct attributes' do
+        lockbox_transaction = reconcile.value.lockbox_transactions.first
+        expect(lockbox_transaction.eff_date).to eq(Date.current)
+        expect(lockbox_transaction.amount).to eq(Monetize.parse('100.00'))
+        expect(lockbox_transaction.balance_effect).to eq(LockboxTransaction::DEBIT)
+      end
+    end
+
     it 'creates one lockbox action' do
       expect{reconcile}.to change(LockboxAction, :count).by(1)
     end
