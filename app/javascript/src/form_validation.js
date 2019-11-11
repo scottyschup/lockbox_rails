dcbbce9a3dd654d0042c88492991875aa80b4d5f
Scott Schupbach <ssschupbach@gmail.com>
2019-11-10 18:35:15 -0600

    Clarify comments; add phone-ext; fix minor bug

--- app/javascript/src/form_validation.js
+++ app/javascript/src/form_validation.js
@@ -1,3 +1,6 @@
+// 1. Add the `.pristine` class to all inputs
+// 2. Add an event listner to remove `.pristine` once an input has been
+// interacted with.
 const addPristineToInputs = () => {
   const inputs = document.querySelectorAll("input, select");
   for (let i = 0; i < inputs.length; i++) {
@@ -11,8 +14,9 @@ const addPristineToInputs = () => {
   }
 }
 
+// Remove `.pristine` from all inputs when submit button is clicked.
 const removePristineFromInputs = () => {
-  const inputs = document.getElementsByTagName("input");
+  const inputs = document.querySelectorAll("input, select");
   for (let i = 0; i < inputs.length; i++) {
     let input = inputs[i];
     input.classList.remove("pristine");
