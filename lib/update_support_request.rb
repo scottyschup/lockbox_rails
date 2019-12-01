219055652fc937892282b8582a307bef6e9c86a6
Chris Doyle <chris@arches.io>
2019-12-01 16:46:04 -0600

    interactor for support request update

---
+++ lib/update_support_request.rb
@@ -0,0 +1,90 @@
+require 'verbalize'
+
+class UpdateSupportRequest
+  include Verbalize::Action
+
+  # This is what params input looks like
+  #
+  # {
+  #   support_request: {
+  #     lockbox_action_attributes: {
+  #       eff_date: "2019-09-04",
+  #       id: "6",
+  #       lockbox_transactions_attributes: {
+  #         "0": {
+  #           amount: "33.16",
+  #           category: "",
+  #           _destroy: "false",
+  #           id: "7"
+  #         },
+  #         "1": {
+  #           ...
+  #         },
+  #       }
+  #     },
+  #     name_or_alias: "Foster",
+  #     client_ref_id: "qwertyuiopp",
+  #     urgency_flag: "one"
+  #   },
+  #   lockbox_partner_id: "3",
+  #   id: "3"
+  # }
+  #
+  # We'll pass it directly to SupportRequest to take advantage of AcceptsNestedAttributes.
+  input :params
+
+  attr_accessor :support_request, :original_values
+
+  def call
+    find_support_request
+
+    cache_original_values
+
+    if support_request.update(params[:support_request])
+      notate_changes
+    end
+
+    support_request
+  end
+
+  private
+
+  def cache_original_values
+    self.original_values ||= {}
+    original_values[:client_ref_id] = support_request.client_ref_id
+    original_values[:name_or_alias] = support_request.name_or_alias
+    original_values[:urgency_flag]  = support_request.urgency_flag
+    original_values[:eff_date]      = support_request.eff_date
+    original_values[:amount]        = support_request.amount
+  end
+
+  def notate_changes
+    note_text = []
+
+    timestamp = Time.now.strftime("%Y-%m-%d at %-I:%M%P")
+
+    original_values.each do |field, original_value|
+      new_value = support_request.send(field)
+      if new_value != original_value
+        note_text << "The #{field_labels[field]} for this Support Request was changed on #{timestamp} from #{original_value} to #{new_value}"
+      end
+    end
+
+    support_request.notes.create(text: note_text.join("\n"))
+  end
+
+  def field_labels
+    HashWithIndifferentAccess.new(
+      amount: "Total Amount",
+      client_ref_id: "Client Reference ID",
+      eff_date: "Pickup Date",
+      name_or_alias: "Client Alias",
+      urgency_flag: "Urgency Flag",
+    )
+  end
+
+  def find_support_request
+    self.support_request = SupportRequest.find(params[:id])
+  end
+
+end
---
+++ spec/lib/update_support_request_spec.rb
@@ -0,0 +1,90 @@
+require 'rails_helper'
+require './lib/update_support_request'
+
+describe UpdateSupportRequest do
+
+  let(:support_request) { FactoryBot.create(:support_request, :pending) }
+  let(:lockbox_transaction) { FactoryBot.create(:lockbox_transaction) }
+
+  before do
+    support_request.lockbox_action.lockbox_transactions = [lockbox_transaction]
+  end
+
+  it 'updates the support request' do
+    expect(support_request).to be_persisted
+    result = UpdateSupportRequest.call(params: {id: support_request.id, support_request: {client_ref_id: '5678'}})
+    expect(result).to be_success
+    expect(result.value).to be_an_instance_of(SupportRequest)
+    expect(result.value.client_ref_id).to eq('5678')
+  end
+
+  it 'creates a note if certain support request fields were changed' do
+    new_values = {
+      client_ref_id: 'new client ref',
+      name_or_alias: 'new name',
+      urgency_flag: 'new urgency',
+    }
+
+    new_values.each do |field, value|
+      expect{UpdateSupportRequest.call(params: {id: support_request.id, support_request: {field => value}})}
+        .to change{Note.count}
+        .by(1)
+    end
+
+  end
+
+  it 'creates a note if the total amount was changed' do
+    update_params = {
+      support_request: {
+        lockbox_action_attributes: {
+          id: support_request.lockbox_action.id,
+          lockbox_transactions_attributes: {
+            "0" => {
+              amount: (lockbox_transaction.amount_cents/100.0 + 100).to_s,
+              category: "",
+              _destroy: "false",
+              id: lockbox_transaction.id
+            },
+          }
+        },
+      },
+      lockbox_partner_id: support_request.lockbox_partner.id,
+      id: support_request.id
+    }
+
+    expect{UpdateSupportRequest.call(params: update_params)}
+      .to change{Note.count}
+      .by(1)
+    expect(Note.last.text).to include("The Total Amount for this Support Request was changed")
+  end
+
+  it 'creates a note if the pickup date has changed' do
+    update_params = {
+      support_request: {
+        lockbox_action_attributes: {
+          eff_date: (support_request.lockbox_action.eff_date + 1.day).strftime("%Y-%m-%d"),
+          id: support_request.lockbox_action.id,
+        },
+      },
+      lockbox_partner_id: support_request.lockbox_partner.id,
+      id: support_request.id
+    }
+
+    expect{UpdateSupportRequest.call(params: update_params)}
+      .to change{Note.count}
+      .by(1)
+    expect(Note.last.text).to include("The Pickup Date for this Support Request was changed")
+  end
+
+  it 'creates a single note if multiple fields changed' do
+      expect{UpdateSupportRequest.call(params: {id: support_request.id, support_request: {client_ref_id: 'new client ref', name_or_alias: 'new name', urgency_flag: 'new urgency'}})}
+        .to change{Note.count}
+        .by(1)
+
+      note = Note.last
+      expect(note.text).to include("The Client Reference ID for this Support Request was changed")
+      expect(note.text).to include("The Client Alias for this Support Request was changed")
+      expect(note.text).to include("The Urgency Flag for this Support Request was changed")
+  end
+end
+
