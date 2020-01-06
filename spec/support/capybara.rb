5cc31f92e4e76535ce6a203c216a09fde2cb75b8
Scott Schupbach <ssschupbach@gmail.com>
2020-01-05 18:22:43 -0600

    Add warning message for unsopported browser drivers

--- spec/support/capybara.rb
+++ spec/support/capybara.rb
@@ -6,12 +6,19 @@ end
 
 RSpec.configure do |config|
   config.before(:each, type: :system) do
-    if ENV['DRIVER']
-      case ENV['DRIVER'].to_sym
+    if ENV['BROWSER']
+      case ENV['BROWSER'].to_sym.downcase
       when :chrome
         driven_by :selenium_chrome
       when :firefox
         driven_by :selenium_firefox
+      else
+        msg = "\n[WARNING] Invalid browser specified: '#{ENV['BROWSER']}'\n"
+        msg += "\tAvailable browsers are 'chrome' or 'firefox'\n"
+        msg += "\tDefaulting to headless Selenium driver\n\n"
+
+        puts msg
+        driven_by :selenium_headless
       end
     else
       driven_by :selenium_headless
