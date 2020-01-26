107e8c024bd2dc109a3f2dfef0d13f3b299c456a
Chris Doyle <chris@arches.io>
2020-01-25 19:38:16 -0600

    remove support request mailer in favor of note mailer
    Since a support request creates a note, we were previously
    sending two emails - one for the note, and one for the
    support request itself. This commit unifies notifications
    into the note mailer.

--- app/mailers/note_mailer.rb
+++ app/mailers/note_mailer.rb
@@ -1,21 +1,45 @@
 class NoteMailer < ApplicationMailer
 
-  def note_creation_alert
-    @note = params[:note]
-
-    if @note.system_generated?
-      subject = "New System note on #{@note.notable_type.titleize} ##{@note.notable_id}"
-    else
-      subject = "New note from #{@note.author} on #{@note.notable_type.titleize} ##{@note.notable_id}"
-    end
-
-    mail(to: params[:address], subject: subject)
-  end
-
   def self.deliver_note_creation_alerts(note)
     recipients(note).collect do |email|
       NoteMailer.with(note: note, address: email).note_creation_alert.deliver_now
     end
+  end
+
+  def note_creation_alert
+    @note = params[:note]
+
+    if @note.notable_type == "SupportRequest" && @note.notable_action == "create"
+      support_request_creation_alert
+    elsif @note.system_generated?
+      system_annotation_alert
+    else
+      user_annotation_alert
+    end
+  end
+
+  def system_annotation_alert
+    subject = "New System note on #{@note.notable_type.titleize} ##{@note.notable_id}"
+    mail(to: params[:address], subject: subject, template_name: "system_annotation_alert")
+  end
+
+  def user_annotation_alert
+    subject = "New note from #{@note.author} on #{@note.notable_type.titleize} ##{@note.notable_id}"
+    mail(to: params[:address], subject: subject, template_name: "user_annotation_alert")
+  end
+
+  def support_request_creation_alert
+    @support_request = @note.notable
+
+    urgency_flag_prefix = if @support_request.urgency_flag.present?
+      "#{@support_request.urgency_flag} - "
+    else
+      ""
+    end
+
+    subject = "#{urgency_flag_prefix}MAC Cash Box Withdrawal Request"
+
+    mail(to: params[:address], subject: subject, template_name: "support_request_creation_alert")
   end
 
   private
--- spec/mailers/note_mailer_spec.rb
+++ spec/mailers/note_mailer_spec.rb
@@ -7,6 +7,9 @@ describe NoteMailer, type: :mailer do
   let(:support_request) do
     FactoryBot.create(:support_request, :pending, lockbox_partner: lockbox_partner)
   end
+  let(:urgent_support_request) do
+    FactoryBot.create(:support_request, :pending, lockbox_partner: lockbox_partner, urgency_flag: "Super Urgent")
+  end
 
   let(:admin_note) do
     FactoryBot.create(:note, user: admin_user, notable: support_request)
@@ -16,6 +19,12 @@ describe NoteMailer, type: :mailer do
   end
   let(:partner_note) do
     FactoryBot.create(:note, user: lockbox_partner.users.first, notable: support_request)
+  end
+  let(:support_request_creation_note) do
+    FactoryBot.create(:note, user: lockbox_partner.users.first, notable: support_request, notable_action: "create")
+  end
+  let(:urgent_support_request_creation_note) do
+    FactoryBot.create(:note, user: lockbox_partner.users.first, notable: urgent_support_request, notable_action: "create")
   end
 
   describe "#note_creation_alert" do
@@ -73,6 +82,52 @@ describe NoteMailer, type: :mailer do
           expect(recipients).to include(address)
         end
       end
+    end
+
+    context "for a support request creation" do
+      context "when there is no urgency flag" do
+        it "has the expected subject line" do
+          support_request_creation_note
+          email = ActionMailer::Base.deliveries.first
+          expect(email.subject).to eq("MAC Cash Box Withdrawal Request")
+        end
+      end
+
+      context "when there is an urgency flag" do
+        it "has the expected subject line" do
+          urgent_support_request_creation_note
+          email = ActionMailer::Base.deliveries.first
+          expect(email.subject).to eq("Super Urgent - MAC Cash Box Withdrawal Request")
+        end
+      end
+
+      context "body" do
+        before do
+          support_request_creation_note
+          @email = ActionMailer::Base.deliveries.first
+        end
+
+        it "includes the pickup date" do
+          expect(@email.body.encoded).to include(
+            support_request.pickup_date.strftime("%A, %B %d, %Y")
+          )
+        end
+
+        it "includes the amount" do
+          expect(@email.body.encoded).to include(support_request.amount.format)
+        end
+
+        it "includes the coordinator's name" do
+          expected_name = ERB::Util.html_escape(support_request.user.name)
+          expect(@email.body.encoded).to include(expected_name)
+        end
+
+        it "includes a link to the support_request" do
+          path = "/lockbox_partners/#{lockbox_partner.id}/support_requests/#{support_request.id}"
+          expect(@email.body.encoded).to include(path)
+        end
+      end
+
     end
   end
 
