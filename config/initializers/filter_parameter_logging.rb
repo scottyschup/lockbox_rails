2871784b8da76bb1d9bea37cadd374e8a3f03a5b
BintLopez <nmlopez21@gmail.com>
2020-02-06 11:59:28 -0600

    clean up; add comments

--- config/initializers/filter_parameter_logging.rb
+++ config/initializers/filter_parameter_logging.rb
@@ -1,4 +1,11 @@
 # Be sure to restart your server when you modify this file.
 
 # Configure sensitive parameters which will be filtered from the log file.
-Rails.application.config.filter_parameters += [:password, :password_confirmation, :current_password, :email, :name_or_alias, :name]
+Rails.application.config.filter_parameters += [
+  # Regex string matching;
+  # will filter any param with the following substrings
+  :password, # :password_confirmation, :current_password
+  :email,
+  :name, # :name_or_alias
+  :text
+]
