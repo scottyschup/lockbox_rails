4777e048b6eb1baaa07c5003bd85ef0e8b2ecaf3
Kevin Yaroch <kjyaroch@gmail.com>
2020-02-16 16:46:27 -0600

    Update factory

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -5,6 +5,7 @@ FactoryBot.define do
     password     { 'superstrongpassword12345' }
     confirmed_at { Time.current }
     role         { 'admin' }
+    time_zone    { 'America/Chicago' }
 
     trait :partner_user do
       role { 'partner' }
