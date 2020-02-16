cf9c1145a51bc1b9272345a87af59a458540dd7d
Kevin Yaroch <kjyaroch@gmail.com>
2020-02-16 16:38:26 -0600

    Add time_zone to users

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_01_26_005338) do
+ActiveRecord::Schema.define(version: 2020_02_16_223630) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -98,6 +98,7 @@ ActiveRecord::Schema.define(version: 2020_01_26_005338) do
     t.datetime "updated_at", precision: 6, null: false
     t.string "role"
     t.bigint "invited_by_id"
+    t.string "time_zone"
     t.index ["confirmation_token"], name: "index_users_on_confirmation_token", unique: true
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["invited_by_id"], name: "index_users_on_invited_by_id"
