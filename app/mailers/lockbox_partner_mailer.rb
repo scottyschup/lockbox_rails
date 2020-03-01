b319753fcad500644975a13b0f32fe7c32795a06
Scott Schupbach <ssschupbach@gmail.com>
2020-03-01 17:56:45 -0600

    Add NSF mailer action; refactor low balance method for readability/consistency

--- app/mailers/lockbox_partner_mailer.rb
+++ app/mailers/lockbox_partner_mailer.rb
@@ -1,8 +1,20 @@
 class LockboxPartnerMailer < ApplicationMailer
-  def low_balance_alert
-    return unless ENV['LOW_BALANCE_ALERT_EMAIL'].present?
+  def insufficient_funds_alert
+    email_address = ENV['LOCKBOX_EMAIL']
+    return unless email_address.present?
+    subject = %Q(
+      [INSUFFICIENT LOCKBOX FUNDS] #{params[:lockbox_partner].name} can't cover
+      pending support requests
+    ).strip
 
-    @lockbox_partner = params[:lockbox_partner]
-    mail(to: ENV['LOW_BALANCE_ALERT_EMAIL'], subject: "[LOW LOCKBOX BALANCE] #{@lockbox_partner.name} needs cash")
+    mail(to: email_address, subject: subject)
+  end
+
+  def low_balance_alert
+    email_address = ENV['LOW_BALANCE_ALERT_EMAIL']
+    return unless email_address.present?
+    subject = "[LOW LOCKBOX BALANCE] #{params[:lockbox_partner].name} needs cash"
+
+    mail(to: email_address, subject: subject)
   end
 end
