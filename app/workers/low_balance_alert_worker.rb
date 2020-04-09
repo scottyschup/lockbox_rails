ade4e72f4a7e406b34292f52c515a49f2af66e93
BintLopez <nmlopez21@gmail.com>
2020-04-09 00:18:08 -0500

    caught a bus with that test

--- app/workers/low_balance_alert_worker.rb
+++ app/workers/low_balance_alert_worker.rb
@@ -2,11 +2,11 @@ class LowBalanceAlertWorker
   include Sidekiq::Worker
 
   def perform(params)
-    return unless alert = params['alert']
+    return unless params['alert'].present?
     partner = LockboxPartner.find_by(id: params['lockbox_partner_id'])
     return unless partner
 
-    case alert
+    case params['alert']
     when 'insufficient_funds'
       LockboxPartnerMailer.with(lockbox_partner: partner).insufficient_funds_alert.deliver_now
     when 'low_balance'
