adf3fba8e37b0fc41b89e75f157d90af2afbe309
Scott Schupbach <ssschupbach@gmail.com>
2020-06-07 20:05:33 -0500

    Fix tests and add more

--- spec/system/navbar_menu_spec.rb
+++ spec/system/navbar_menu_spec.rb
@@ -1,31 +1,65 @@
 require 'rails_helper'
 
 RSpec.describe "Navbar menu", type: :system do
-  let!(:user) { FactoryBot.create(:user) }
+  let!(:admin_user) { FactoryBot.create(:user), role: User::ADMIN }
+  let!(:lockbox_partner) { create(:lockbox_partner, :active) }
+  let!(:partner_user) { FactoryBot.create(:user, role: User::PARTNER, lockbox_partner: lockbox_partner) }
 
-  before do
-    login_as(user, :scope => :user)
-    visit("/")
-  end
+  describe "when user is an admin" do
+    before do
+      login_as(admin_user)
+      visit("/")
+    end
 
-  context "navbar control button" do
-    it "is present" do
-      page.assert_selector("#navbar-control", visible: :visible, count: 1)
+    context "navbar control button" do
+      it "is present" do
+        page.assert_selector("#navbar-control", visible: :visible, count: 1)
+      end
+    end
+
+    context "navbar drawer" do
+      it "is not visible on page load" do
+        page.assert_no_selector("#navbar-drawer")
+      end
+    end
+
+    context "clicking navbar control button" do
+      it "opens navbar when closed" do
+        click_button("navbar-control")
+        page.assert_selector("#navbar-drawer", visible: :visible, count: 1)
+      end
+
+      it "closes navbar when opened" do
+        click_button("navbar-control")
+        page.assert_selector("#navbar-drawer", visible: :visible, count: 1)
+        click_button("navbar-control")
+        page.assert_no_selector("#navbar-drawer")
+      end
+
     end
   end
 
-  context "clicking navbar control button" do
-    it "opens navbar when closed" do
+  describe "when user is a partner" do
+    before do
+      login_as(partner_user)
+      visit("/")
       click_button("navbar-control")
-      page.assert_selector("#navbar", visible: :visible, count: 1)
     end
 
-    it "closes navbar when opened" do
-      click_button("navbar-control")
-      page.assert_selector("#navbar", visible: :visible, count: 1)
-      click_button("navbar-control")
-      page.assert_selector("#navbar", visible: :hidden, count: 1)
-    end
+    context "when the drawer is open" do
+      it "clicking a secondary nav tab closes the drawer" do
+        click_link("All Activity")
+        page.assert_no_selector("#navbar-drawer")
+      end
 
+      it "clicking multiple secondary nav tabs doesn't cause the drawer to flicker" do
+        click_link("All Activity")
+        click_link("Action Needed")
+        page.assert_no_selector("#navbar-drawer")
+        click_link("All Activity")
+        page.assert_no_selector("#navbar-drawer")
+      end
+    end
   end
+
 end
