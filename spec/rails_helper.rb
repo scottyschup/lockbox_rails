38866aae20a223390d20f945a6793543f67adb7c
Sree Prasad <sreeprasad285@gmail.com>
2020-06-28 14:54:26 -0700

    Adding groups

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -1,6 +1,12 @@
 require 'simplecov'
 SimpleCov.start do
-  add_filter "/spec/"
+  add_filter '/spec/'
+  add_filter '/config/'
+
+  add_group 'Controllers', 'app/controllers'
+  add_group 'Models', 'app/models'
+  add_group 'Helpers', 'app/helpers'
+  add_group 'Libraries', 'lib'
 end
 
 # This file is copied to spec/ when you run 'rails generate rspec:install'
